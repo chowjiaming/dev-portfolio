88f8b9f5238fde5520d232ad632ca46c4845fe5d
Joefs <joefstack@gmail.com>
2022-05-19 23:27:20 -0400

    :memo: added new flashcard project :memo:

--- src/config/projects.js
+++ src/config/projects.js
@@ -1,5 +1,5 @@
 import tipCalculatorScreenshot from "../assets/screenshots/tip-calculator-screenshot.png";
-import calculatorScreenshot from "../assets/screenshots/calculator-screenshot.png";
+import flashcardsScreenshot from "../assets/screenshots/1000words-screenshot.png";
 import countriesScreenshot from "../assets/screenshots/countries-screenshot.png";
 
 export const projects = [
@@ -14,22 +14,22 @@ export const projects = [
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
-    name: "Calculator App",
-    screenshot: { calculatorScreenshot },
-    github: "https://github.com/chowjiaming/calculator-app-react",
-    link: "https://frontend-mentor-calculator-app-chowjiaming.netlify.app/",
-    shortDesc:
-      "Calculator with multiple differnt colour themes, featuring current operator display. Built with React.",
-    longDesc:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  },
-  {
     name: "Countries Info App",
     screenshot: { countriesScreenshot },
     github: "https://github.com/chowjiaming/where-in-the-world",
     link: "https://where-in-the-world-by-chowjiaming.netlify.app/",
     shortDesc:
       "Country info app that lets users filter and search for basic countries information. Built with React and TypeScript.",
+    longDesc:
+      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    name: "Languages Flashcard App",
+    screenshot: { flashcardsScreenshot },
+    github: "https://github.com/chowjiaming/1000WORDS",
+    link: "https://onethousandwords.netlify.app/",
+    shortDesc:
+      "A flashcards app built to help the user get their feet wet learning a new language. Built with React and TypeScript.",
     longDesc:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
